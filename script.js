34fbb2fc832b9d84688f48e399307127e610621e
Samandar <shomurodovhusan130@gmail.com>
2025-03-27 14:11:27 +0500

    yengi

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     const items = document.querySelectorAll(".item");
+    const cards = document.querySelectorAll(".card"); 
 
     items.forEach(item => {
-        
         item.addEventListener("click", function () {
+            
+            const color = this.getAttribute("data-color");
+
+         
+            cards.forEach(card => {
+                card.style.backgroundColor = color;
+            });
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".item2");
+
+    items.forEach(item2 => {
+        
+        item2.addEventListener("click", function () {
 
             items.forEach(i => i.classList.remove("active"));
 
-            item.classList.add('active');
+            item2.classList.add('active');
 
             document.body.style.backgroundColor = this.getAttribute("data-color");
         })
